fa5630f4a4a5c7a36c87d085a354929fd1a85b6e
leormston <l.e.ormston@gmail.com>
2025-12-07 10:50:23 +0000

    adding state file

--- main.tf
+++ main.tf
@@ -1,6 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket = "louie-terraform-test-bucket-predemo-123"
+    key    = "terraform.tfstate"
+    region = "eu-west-2"
+  }
+}
+
 provider "aws" {
   region = "eu-west-2"
 }
+
+  
 
 resource "aws_s3_bucket" "example" {
   bucket = "louie-ormston-terraform-demo-bucket"
